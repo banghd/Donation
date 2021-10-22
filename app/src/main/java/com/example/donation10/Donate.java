f1bd83ccee171a42723492a6fa7eb32e8c374596
bangdm <bangdm@foobla.com>
2021-10-22 16:50:02 +0700

    week 3

--- app/src/main/java/com/example/donation10/Donate.java
+++ app/src/main/java/com/example/donation10/Donate.java
@@ -8,13 +8,17 @@ import android.widget.Button;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.widget.RadioGroup;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class Donate extends AppCompatActivity {
     private Button donateButton;
-
+    private RadioGroup paymentMethod;
+    private ProgressBar progressBar;
+    private NumberPicker amountPicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +39,9 @@ public class Donate extends AppCompatActivity {
         {
             Log.v("Donate", "Really got the donate button");
         }
-
+        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,6 +60,10 @@ public class Donate extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void donateButtonPressed(View view) {
+        Log.v("Donate", "Donate Pressed!");
     }
 
 }
--- app/src/main/java/com/example/donation10/MainActivity.java
+++ app/src/main/java/com/example/donation10/MainActivity.java
@@ -8,13 +8,15 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button donateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +32,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        donateButton = (Button) findViewById(R.id.donateButton);
+        if (donateButton != null)
+        {
+            Log.v("Donate", "Really got the donate button");
+        }
     }
-
+    public void donateButtonPressed (View view)
+    {
+        Log.v("Donate", "Donate Pressed!");
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
